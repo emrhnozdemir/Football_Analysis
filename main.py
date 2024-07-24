c1f4d216ab233342a626ae6fff09562428b67e8d
Emirhan Ozdemir <66160721+emrhnozdemir@users.noreply.github.com>
2024-07-24 22:06:31 +0300

    object detection and tracking

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from utils import read_video, save_video
+from trackers import Tracker
+
 def main():
-    print("Hello World!")
+  video_frames = read_video('input_videos/08fd33_4.mp4')
+
+  tracker = Tracker('models/best.pt')
+  tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1') 
+
+  output_video_frames = tracker.draw_annotations(video_frames, tracks)
+  save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == '__main__':
-    main()
+  main()
--- yolo_inference.py
+++ yolo_inference.py
@@ -7,4 +7,4 @@ result = model.predict('input_videos/08fd33_4.mp4', save = True)
 print(result[0])
 print("===========================================")
 for box in result[0].boxes:
-    print(box)
+  print(box)
