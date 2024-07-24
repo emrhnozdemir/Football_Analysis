d98ed3b9adc9612befa79ab4451179c192adf221
Emirhan Ozdemir <66160721+emrhnozdemir@users.noreply.github.com>
2024-07-25 00:00:48 +0300

    interpolating the ball

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import numpy as np
 from utils import read_video, save_video
 from trackers import Tracker
 from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+
 
 def main():
   video_frames = read_video('input_videos/08fd33_4.mp4')
 
   tracker = Tracker('models/best.pt')
   tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1') 
+
+  tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
   team_assigner = TeamAssigner()
   team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
@@ -17,7 +22,23 @@ def main():
       tracks['players'][frame_num][player_id]['team'] = team 
       tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-  output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+  player_assigner = PlayerBallAssigner()
+  team_ball_control= []
+  for frame_num, player_track in enumerate(tracks['players']):
+    ball_bbox = tracks['ball'][frame_num][1]['bbox']
+    assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+    if assigned_player != -1:
+      tracks['players'][frame_num][assigned_player]['has_ball'] = True
+      team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+    else:
+      team_ball_control.append(team_ball_control[-1])
+  
+  team_ball_control= np.array(team_ball_control)
+
+
+  output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
   save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == '__main__':
