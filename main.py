b656cf443d9e74e324e77c49de4b8b2460d0958c
Emirhan Ozdemir <66160721+emrhnozdemir@users.noreply.github.com>
2024-07-25 07:10:46 +0300

    camera movement estimator

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import read_video, save_video
 from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
+from camera_movement_estimator import CameraMovementEstimator
 
 
 def main():
@@ -10,6 +11,11 @@ def main():
 
   tracker = Tracker('models/best.pt')
   tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1') 
+  tracker.add_position_to_tracks(tracks)
+
+  camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+  camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames, read_from_stub = True, stub_path = 'stubs/camera_movement_stub.pkl')
+  camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
 
   tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
@@ -39,6 +45,7 @@ def main():
 
 
   output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
+  output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
   save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == '__main__':
