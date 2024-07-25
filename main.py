db0f55ae004abf92b675c23901e0ec6dce6d9ea9
Emirhan Ozdemir <66160721+emrhnozdemir@users.noreply.github.com>
2024-07-25 07:34:53 +0300

    perspective transformer

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
-
+from view_transformer import ViewTransformer
 
 def main():
   video_frames = read_video('input_videos/08fd33_4.mp4')
@@ -16,6 +16,9 @@ def main():
   camera_movement_estimator = CameraMovementEstimator(video_frames[0])
   camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames, read_from_stub = True, stub_path = 'stubs/camera_movement_stub.pkl')
   camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
+  view_transformer = ViewTransformer()
+  view_transformer.add_transformed_position_to_tracks(tracks)
 
   tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
